6414d2df45c0a28e6bbf49147567c39992c945ff
mudjaycker <tombutoyi@gmail.com>
2021-06-15 00:27:49 +0200

    putting in main function

--- GUI_binHex.py
+++ GUI_binHex.py
@@ -1,7 +1,5 @@
-##########################################################################
 ############################# Convert to #################################
 ############################## Binary ###################################
-##########################################################################
 def varToBin(variable):
     
     tabBin = [int(ord(i)) for i in variable]
@@ -9,31 +7,29 @@ def varToBin(variable):
     binary = ' '.join(str(i) for i in binaire)
     
     return binary
-
+############################# Convert to #################################
+############################## Hexadecimal ###############################
 def VarToHex(variable):
     tabHex = [int(ord(i)) for i in variable]
     hexadecimal = [format(i,'X') for i in tabHex]
     hexa = ' '.join(str(i) for i in hexadecimal)
     return hexa
-    
+
     
 
-##########################################################################
-############################# Graphique #################################
-############################## Binary ###################################
-############################### HEXA ####################################
+############################# GUI #################################
 
 from tkinter import *
-############################## Fonctions de conversion ###################################
+############################## Conversion functions ###################################
 def hexBinUpdate(*args):
         toConvert = valueInput.get()
-        
+
         binOutput.set(varToBin(toConvert))
         hexOutput.set(VarToHex(toConvert))
         if toConvert == "":
             binOutput.set("")
             hexOutput.set("")
-
+############################## onCenter function  ###################################
 def onCenter(root:str, W:int, H:int):
     screenX = root.winfo_screenwidth()
     scrennY = root.winfo_screenheight()
@@ -44,60 +40,68 @@ def onCenter(root:str, W:int, H:int):
 
     return root.geometry(geo)
 
-root = Tk()
-W= 760
-H = 365
-onCenter(root, W, H)
-root.resizable(False,False)
+
+ ############################## run function (main function) ###################################
+def run():
+    root = Tk()
+    W= 760
+    H = 365
+    onCenter(root, W, H)
+    root.resizable(False,False)
 
 
 
-valueInput = StringVar()
-valueInput.trace('w',hexBinUpdate)
-entry = Entry(root,width=80,textvariable=valueInput).place(y=80,x=50)
+    valueInput = StringVar()
+    valueInput.trace('w',hexBinUpdate)
+    entry = Entry(root,width=80,textvariable=valueInput).place(y=80,x=50)
 
-labBinPos = W/3.415
-binOutput = StringVar()
-labelBin  = Label(root,bg="#deb887",width=80,textvariable=binOutput)
-labelBin.config(font=('Andika',11))
-labelBin.place(y=labBinPos,x=15)
-
-
-labHexPos = W/2.732
-
-hexOutput = StringVar()
-labelHex = Label(root,bg="#a9a9a9",width=80,textvariable=hexOutput)
-labelHex.config(font=('Andika',11))
-labelHex.place(y=labHexPos,x=15)
+    labBinPos = W/3.415
+    binOutput = StringVar()
+    labelBin  = Label(root,bg="#deb887",width=80,textvariable=binOutput)
+    labelBin.config(font=('Andika',11))
+    labelBin.place(y=labBinPos,x=15)
 
 
+    labHexPos = W/2.732
 
-################### Fonctions to copy ######################
-from tkinter import messagebox
-
-def copyBin():
-    value = valueInput.get()
-    root.clipboard_clear()
-    root.clipboard_append(varToBin(value))
-    messagebox.showinfo("Binary copied!","{} copied".format(varToBin(value)))
-
-def copyHex():
-    value = valueInput.get()
-    root.clipboard_clear()
-    root.clipboard_append(VarToHex(value))
-    messagebox.showinfo("Hexadecimal copied!","{} copied".format(VarToHex(value)))
+    hexOutput = StringVar()
+    labelHex = Label(root,bg="#a9a9a9",width=80,textvariable=hexOutput)
+    labelHex.config(font=('Andika',11))
+    labelHex.place(y=labHexPos,x=15)
 
 
 
+    ################### Fonctions to copy ######################
+    from tkinter import messagebox
 
-butY=int(H/(H/310))
-but = int(W/(W/20))
-butBin = Button(root,width=2,height=1,bg='#deb887',command=copyBin,
-                text="copy").place(y=butY,x=but)
+    def copyBin():
+        value = valueInput.get()
+        root.clipboard_clear()
+        root.clipboard_append(varToBin(value))
+        messagebox.showinfo("Binary copied!","{} copied".format(varToBin(value)))
 
-butX=int(W/(W/650))
-butHex = Button(root,width=2,height=1,bg='#a9a9a9',command=copyHex,
-                text="copy").place(y=butY, x=butX)
+    def copyHex():
+        value = valueInput.get()
+        root.clipboard_clear()
+        root.clipboard_append(VarToHex(value))
+        messagebox.showinfo("Hexadecimal copied!","{} copied".format(VarToHex(value)))
 
 
-root.mainloop()
+
+
+    butY=int(H/(H/310))
+    but = int(W/(W/20))
+    butBin = Button(root,width=2,height=1,bg='#deb887',command=copyBin,
+                    text="copy").place(y=butY,x=but)
+
+    butX=int(W/(W/650))
+    butHex = Button(root,width=2,height=1,bg='#a9a9a9',command=copyHex,
+                    text="copy").place(y=butY, x=butX)
+    root.mainloop()
+    
+
+
+
+
+if __name__=="__main__":
+    run()
